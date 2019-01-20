6d5433cb9c47626ebb23bbb1ee7dcc9ec14278c4
nikkieto <46796781+nikkieto@users.noreply.github.com>
2019-01-20 12:54:33 -0500

    Oculus working

---
+++ Assets/Nikkie/Cancer Cells/Enemy Cancer Cells/EnemyMovement/EnemyMovement.cpp
@@ -0,0 +1,49 @@
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+#include "pch.h"
+#include <iostream>
+
+using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehavior {
+
+	Transform player;				//Player's position.
+	PlayerHealth playerHealth;		//Player's health.
+	EnemyHealth enemy health;		//Enemy's health.
+	NavMesAgent nav;				//Nav mesh agent.
+
+	void Awake() {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		playerHealth = player.GetComponent <PlayerHealth> ();
+		enemyHealth = GetComponent <EnemyHealth> ();
+		nav = GetComponent <NavMeshAgent>();
+	}
+
+	void Update() {
+		if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0) { // If the enemy and the player have health left...
+			nav.SetDestination(player.position);							  // ... set the destination of the nav mesh agent to the player.
+		}
+		else {
+			nav.enabled = false;
+		}
+	}
+}
+
+
+
+	/*int main(){
+		std::cout << "Hello World!\n";
+	}*/
+
+
+};
---
+++ Assets/Nikkie/Cancer Cells/Enemy Cancer Cells/EnemySpawn/EnemySpawn.cpp
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehavior {
+
+    public PlayerHealth playerHealth;   //Player's health.
+    public GameObject enemy;            //Enemy prefab to be spawned.
+    public float spawnTime = 3f;        //Spawn delay wait time.
+    public Transform[] spawnPoints;     //Stores the coordinates of enemy spawn points.
+    
+    void start(){
+        InvokeRepeating("Spawn", spawnTime; spawnTime);
+    }
+
+    void spawn() {
+        if (playerHealth.currentHealth <= 0f) {  //if player has no more health
+            return;
+        }
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.length);  //Generate random number between 0 and total number of spawn points
+
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+ }
